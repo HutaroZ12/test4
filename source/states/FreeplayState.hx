c3a2d35ce8838f58faa78c84bb1a71420db7f45d
Hu <146780983+HutaroZ12@users.noreply.github.com>
2025-11-16 16:01:53 -0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -168,79 +168,161 @@ if(allowErect && !songs[i].hasErect) {
 			// using a FlxGroup is too much fuss!
 			iconArray.push(icon);
 			add(icon);
+override function create()
+{
+    persistentUpdate = true;
+    PlayState.isStoryMode = false;
+    WeekData.reloadWeekFiles(false);
 
-			// songText.x += 40;
-			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
-			// songText.screenCenter(X);
-		}
-		WeekData.setDirectoryFromWeek();
+    #if DISCORD_ALLOWED
+    DiscordClient.changePresence("In the Menus", null);
+    #end
 
-		scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
-		scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
+    final accept:String = (controls.mobileC) ? "A" : "ACCEPT";
+    final reject:String = (controls.mobileC) ? "B" : "BACK";
 
-		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0xFF000000);
-		scoreBG.alpha = 0.6;
-		add(scoreBG);
+    if(WeekData.weeksList.length < 1)
+    {
+        FlxTransitionableState.skipNextTransIn = true;
+        persistentUpdate = false;
+        MusicBeatState.switchState(new states.ErrorState(
+            "NO WEEKS ADDED FOR FREEPLAY\n\nPress " + accept + " to go to the Week Editor Menu.\nPress " + reject + " to return to Main Menu.",
+            function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
+            function() MusicBeatState.switchState(new states.MainMenuState())
+        ));
+        return;
+    }
 
-		diffText = new FlxText(scoreText.x, scoreText.y + 36, 0, "", 24);
-		diffText.font = scoreText.font;
-		add(diffText);
+    // Carrega todas as músicas
+    for (i in 0...WeekData.weeksList.length)
+    {
+        if(weekIsLocked(WeekData.weeksList[i])) continue;
 
-		add(scoreText);
+        var leWeek:WeekData = WeekData.weeksLoaded.get(WeekData.weeksList[i]);
 
+        for (song in leWeek.songs)
+        {
+            var colors:Array<Int> = song[2];
+            if(colors == null || colors.length < 3) colors = [146,113,253];
 
-		missingTextBG = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		missingTextBG.alpha = 0.6;
-		missingTextBG.visible = false;
-		add(missingTextBG);
-		
-		missingText = new FlxText(50, 0, FlxG.width - 100, '', 24);
-		missingText.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		missingText.scrollFactor.set();
-		missingText.visible = false;
-		add(missingText);
+            var folder = song[0];
+            var meta:SongMetadata = new SongMetadata(
+                song[0], i, song[1], FlxColor.fromRGB(colors[0], colors[1], colors[2]), folder
+            );
 
-		if(curSelected >= songs.length) curSelected = 0;
-		bg.color = songs[curSelected].color;
-		intendedColor = bg.color;
-		lerpSelected = curSelected;
+            // Filtra Erect
+            if (allowErect && !songHasErect(meta))
+                meta.hasErect = false;
 
-		curDifficulty = Math.round(Math.max(0, Difficulty.defaultList.indexOf(lastDifficultyName)));
+            // Adiciona apenas se for jogável
+            if (!allowErect || meta.hasErect)
+                songs.push(meta);
+        }
+    }
 
-		bottomBG = new FlxSprite(0, FlxG.height - 26).makeGraphic(FlxG.width, 26, 0xFF000000);
-		bottomBG.alpha = 0.6;
-		add(bottomBG);
+    // --- Configuração visual e inicialização ---
+    bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+    bg.antialiasing = ClientPrefs.data.antialiasing;
+    add(bg);
+    bg.screenCenter();
 
-		final space:String = (controls.mobileC) ? "X" : "SPACE";
-		final control:String = (controls.mobileC) ? "C" : "CTRL";
-		final reset:String = (controls.mobileC) ? "Y" : "RESET";
-		
-		var leText:String = Language.getPhrase("freeplay_tip", "Press {1} to listen to the Song / Press {2} to open the Gameplay Changers Menu / Press {3} to Reset your Score and Accuracy.", [space, control, reset]);
-		bottomString = leText;
-		var size:Int = 16;
-		bottomText = new FlxText(bottomBG.x, bottomBG.y + 4, FlxG.width, leText, size);
-		bottomText.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, CENTER);
-		bottomText.scrollFactor.set();
-		add(bottomText);
-		
-		player = new MusicPlayer(this);
-		add(player);
-		
-		changeSelection();
-		updateTexts();
+    grpSongs = new FlxTypedGroup<Alphabet>();
+    add(grpSongs);
 
-		addTouchPad('LEFT_FULL', 'A_B_C_X_Y_Z');
-		super.create();
-	}
+    for (i in 0...songs.length)
+    {
+        var songText:Alphabet = new Alphabet(90, 320, songs[i].songName, true);
+        songText.targetY = i;
+        grpSongs.add(songText);
 
-	override function closeSubState()
-	{
-		changeSelection(0, false);
-		persistentUpdate = true;
-		super.closeSubState();
-		removeTouchPad();
-		addTouchPad('LEFT_FULL', 'A_B_C_X_Y_Z');
-	}
+        // Bloqueia músicas sem Erect
+        if(allowErect && !songs[i].hasErect)
+        {
+            songText.alpha = 0.3;
+            songText.active = false;
+        }
+        else
+        {
+            songText.alpha = 1;
+            songText.active = true;
+        }
+
+        songText.scaleX = Math.min(1, 980 / songText.width);
+        songText.snapToPosition();
+
+        var icon:HealthIcon = new HealthIcon(songs[i].songCharacter);
+        icon.sprTracker = songText;
+        icon.visible = icon.active = false;
+        iconArray.push(icon);
+        add(icon);
+
+        songText.visible = songText.active = songText.isMenuItem = false;
+    }
+
+    WeekData.setDirectoryFromWeek();
+
+    // --- Score e textos ---
+    scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
+    scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
+
+    scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0xFF000000);
+    scoreBG.alpha = 0.6;
+    add(scoreBG);
+
+    diffText = new FlxText(scoreText.x, scoreText.y + 36, 0, "", 24);
+    diffText.font = scoreText.font;
+    add(diffText);
+
+    add(scoreText);
+
+    missingTextBG = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+    missingTextBG.alpha = 0.6;
+    missingTextBG.visible = false;
+    add(missingTextBG);
+
+    missingText = new FlxText(50, 0, FlxG.width - 100, '', 24);
+    missingText.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+    missingText.scrollFactor.set();
+    missingText.visible = false;
+    add(missingText);
+
+    if(curSelected >= songs.length) curSelected = 0;
+    bg.color = songs[curSelected].color;
+    intendedColor = bg.color;
+    lerpSelected = curSelected;
+
+    curDifficulty = Math.round(Math.max(0, Difficulty.defaultList.indexOf(lastDifficultyName)));
+
+    // --- Bottom text ---
+    bottomBG = new FlxSprite(0, FlxG.height - 26).makeGraphic(FlxG.width, 26, 0xFF000000);
+    bottomBG.alpha = 0.6;
+    add(bottomBG);
+
+    final space:String = (controls.mobileC) ? "X" : "SPACE";
+    final control:String = (controls.mobileC) ? "C" : "CTRL";
+    final reset:String = (controls.mobileC) ? "Y" : "RESET";
+
+    var leText:String = Language.getPhrase("freeplay_tip",
+        "Press {1} to listen to the Song / Press {2} to open the Gameplay Changers Menu / Press {3} to Reset your Score and Accuracy.",
+        [space, control, reset]
+    );
+
+    bottomString = leText;
+    var size:Int = 16;
+    bottomText = new FlxText(bottomBG.x, bottomBG.y + 4, FlxG.width, leText, size);
+    bottomText.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, CENTER);
+    bottomText.scrollFactor.set();
+    add(bottomText);
+
+    player = new MusicPlayer(this);
+    add(player);
+
+    changeSelection();
+    updateTexts();
+
+    addTouchPad('LEFT_FULL', 'A_B_C_X_Y_Z');
+    super.create();
+}
 
 	public function addSong(songName:String, weekNum:Int, songCharacter:String, color:Int, folder:String)
 {
