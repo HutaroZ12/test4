fadbae222810cb4fc747d0b4ccddab8ff2e07f50
Hu <146780983+HutaroZ12@users.noreply.github.com>
2025-11-16 00:48:37 -0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -361,6 +361,7 @@ public static function destroyFreeplayVocals() {
 
     if(opponentVocals != null) opponentVocals.stop();
     opponentVocals = FlxDestroyUtil.destroy(opponentVocals);
+     }
 }
 
 class SongMetadata
