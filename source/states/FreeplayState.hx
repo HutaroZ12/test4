86a78544d8781ed4cd2e35963f36c1acc4f24e36
Hu <146780983+HutaroZ12@users.noreply.github.com>
2025-12-07 09:50:27 -0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -569,7 +569,7 @@ class FreeplayState extends MusicBeatState
 			curDifficulty = 0;
 		}
 
-		if (songs[curSelected].songName.toLowerCase()) && Song.hasDifficulty(songs[curSelected].songName.toLowerCase()), 'erect'))
+		if (songs[curSelected].songName.toLowerCase()) && Song.hasDifficulty(songs[curSelected].songName.toLowerCase()), 'erect')
 		{
 			Difficulty.difficulties.push('erect');
 			Difficulty.difficulties.push('nightmare');
