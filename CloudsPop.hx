f1c1d8ec85b22e1f9ae77395c9444c0d1d822bec
Hu <146780983+HutaroZ12@users.noreply.github.com>
2026-02-13 00:39:21 -0300

    Delete CloudsPop.hx

--- CloudsPop.hx
+++
@@ -1,7 +0,0 @@
-// Updated blend mode for background clouds
-class CloudsPop {
-    // Other properties and methods
-    function changeBackgroundBlendMode() {
-        this.blendMode = 'SCREEN'; // Changed from OVERLAY to SCREEN
-    }
-}
